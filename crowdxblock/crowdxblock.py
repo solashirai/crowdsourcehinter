10d1c1f27db78288ea79301433f940ff76276b2b
Sola <sola@mit.edu>
2014-07-10 16:23:06 -0400

    version with error in trying to get ID. just uploading for reference.

--- crowdxblock/crowdxblock.py
+++ crowdxblock/crowdxblock.py
@@ -13,10 +13,10 @@ log = logging.getLogger(__name__)
 #get_hint and get_feedback are in 
 class CrowdXBlock(XBlock):
     correctanswer = String(default="42", scope=Scope.content) #should be irrelevant for completed version
-    hints = Dict(default={"75": {"hint12":10, "hint22":0, "hints32":0}, "1": {"hint1":1, "hint2":1, "hint3":0}, "roflcopter": {"HighFyve":1000}}, scope=Scope.content) #All hints. sorted by type of mistake. type_of_incorrect_answer{"hint":rating, "hint":rating}
+    hints = Dict(default={"2": {"hint1for2":0, "hint2for2":0, "hints3for22":0}, "1": {"hint1for1":0, "hint2for1":0, "hint3for1":0}, "3": {"hint1for3":0, "hint2for3":0, "hint3for3":0}}, scope=Scope.content) #All hints. sorted by type of mistake. type_of_incorrect_answer{"hint":rating, "hint":rating}
     HintsToUse = Dict(default={}, scope=Scope.user_state) #Dict of hints to provide user
     WrongAnswers = List(default=[], scope=Scope.user_state) #List of mistakes made by user
-    DefaultHints = Dict(default={"hint": 100, "hinttwo": 10, "hintthree": 0, "hintasdf": 50, "aas;dklfj?": 1000, "SuperDuperBestHint": 10000}, scope=Scope.content) #Default hints in case no incorrect answers in hints match the user's mistake
+    DefaultHints = Dict(default={"defaulthint1": 0, "defaulthint2": 0, "defaulthint3": 0, "defaulthint4": 0, "defaulthint5": 0, "bestdefaulthint": 3}, scope=Scope.content) #Default hints in case no incorrect answers in hints match the user's mistake
     Used = List(default=[], scope=Scope.user_state)#List of used hints from HintsToUse
     Voted = Integer(default=0, scope=Scope.user_state)#prevent multiple votes/hint submission
 
@@ -73,15 +73,17 @@ class CrowdXBlock(XBlock):
 	    for i in range(0, len(self.Used)):
 		ans = str('wngans' + str(i))
 		hnt = str('hntusd' + str(i))
-		feedbackdict[ans] = str(self.WrongAnswers[i])
-		feedbackdict[hnt] = str(self.Used[i])
+		feedbackdict[str(self.Used[i])] = str(self.WrongAnswers[i])
+	    print str(feedbackdict)
 	    return feedbackdict
     
     @XBlock.json_handler #add 1 or -1 to rating of a hint
     def rate_hint(self, data, suffix=''):
+	ansnum = self.Used.index(str(data['ansnum']))
+	print("ansnum is" +  ansnum)
 	if self.Voted == 0:
             for key in self.DefaultHints:	
-                if key == self.Used[int(data['ansnum'])]: #rating for hints in DefaultHints
+                if key == self.Used[int(ansnum)]: #rating for hints in DefaultHints
 	            self.DefaultHints[str(key)] += int(data["rating"])
 		    self.Voted = 1
 		    print str(self.DefaultHints)
@@ -89,8 +91,8 @@ class CrowdXBlock(XBlock):
             for key in self.hints:
 	        tempdict = str(self.hints[str(key)]) #rate hint that is in hints 
 	        tempdict = (ast.literal_eval(tempdict))
-	        if str(key) == str(self.WrongAnswers[data['ansnum']]): #ansnum will the the answer/hint pair that is selected 
-		    tempdict[self.Used[int(data['ansnum'])]] += int(data["rating"])
+	        if str(key) == str(self.WrongAnswers[ansnum]): #ansnum will the the answer/hint pair that is selected 
+		    tempdict[self.Used[int(ansnum)]] += int(data["rating"])
 		    self.hints[str(key)] = tempdict
 		    print("TESTING AGAIN HI")
 		    print("hints are " + str(self.hints[str(key)]))
