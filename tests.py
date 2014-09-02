ec4defe59e98be1846eee984bd9efdf2de32d6e6
Sola <sola@mit.edu>
2014-09-02 21:14:27 +0900

    made a simple test case, maybe correctly

--- tests.py
+++ tests.py
@@ -1,2 +1,14 @@
-def test_
-
+def test_rate_hint(self):
+    '''
+    This test should test the rate_hint in crowdxblock,
+    in the event that the rating increases by 1.
+    '''
+    resp = self.call_event(
+        'rate_hint', {
+            'student_rating': 1,
+            'value': 'hint',
+            'answer': 'answer
+        }
+    )
+    self.assertEqual(resp['Success'], True)
+    self.assertEqual(resp['rating'], 2)
